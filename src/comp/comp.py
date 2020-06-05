c3009988fc94a7246a46f8f1f0731789c9720064
Annette Odingo <odingoannette@gmail.com>
2020-06-05 08:54:58 +0100

    comp part 5

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -44,28 +44,28 @@ print("Ages plus 10:")
 d = [human.age + 10 for human in humans]
 print(d)
 
-# # Write a list comprehension that creates a list of strings which are the name
-# # joined to the age with a hyphen, for example "David-31", for all humans.
-# print("Name hyphen age:")
-# e = []
-# print(e)
+# Write a list comprehension that creates a list of strings which are the name
+# joined to the age with a hyphen, for example "David-31", for all humans.
+print("Name hyphen age:")
+e = [f'{human.name}-{human.age}' for human in humans]
+print(e)
 
-# # Write a list comprehension that creates a list of tuples containing name and
-# # age, for example ("David", 31), for everyone between the ages of 27 and 32,
-# # inclusive.
-# print("Names and ages between 27 and 32:")
-# f = []
-# print(f)
+# Write a list comprehension that creates a list of tuples containing name and
+# age, for example ("David", 31), for everyone between the ages of 27 and 32,
+# inclusive.
+print("Names and ages between 27 and 32:")
+f = []
+print(f)
 
-# # Write a list comprehension that creates a list of new Humans like the old
-# # list, except with all the names uppercase and the ages with 5 added to them.
-# # The "humans" list should be unmodified.
-# print("All names uppercase:")
-# g = []
-# print(g)
+# Write a list comprehension that creates a list of new Humans like the old
+# list, except with all the names uppercase and the ages with 5 added to them.
+# The "humans" list should be unmodified.
+print("All names uppercase:")
+g = []
+print(g)
 
-# # Write a list comprehension that contains the square root of all the ages.
-# print("Square root of ages:")
-# import math
-# h = []
-# print(h)
+# Write a list comprehension that contains the square root of all the ages.
+print("Square root of ages:")
+import math
+h = []
+print(h)
