62b187f25717d6dd4f36e29e12c15b798a1aaaf2
Annette Odingo <odingoannette@gmail.com>
2020-06-05 09:20:45 +0100

    comp Fix bugs

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -24,19 +24,19 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = [human for human in humans if human.name[0] == "D" ]
+a = [human.name for human in humans if human.name[0] == "D" ]
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = [human for human in humans if human.name[-1] == "e" ]
+b = [human.name for human in humans if human.name[-1] == "e" ]
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = [human for human in  humans  if human.name[0]  in  ["C", "D", "E", "F", "G"]]
+c = [human.name for human in  humans  if human.name[0]  in  ["C", "D", "E", "F", "G"]]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
@@ -62,7 +62,7 @@ print(f)
 # The "humans" list should be unmodified.
 print("All names uppercase:")
 # Human("David", 31),
-g = [ f'Human{tuple([human.name.upper(), human.age + 5])}' for human in humans]
+g = [ f"Human{tuple([human.name.upper(), human.age + 5])}" for human in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
