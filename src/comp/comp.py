1ac742c3381acafde07d13fc940dacbf023bbc7b
Annette Odingo <odingoannette@gmail.com>
2020-06-05 08:59:15 +0100

    comp part 6

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -47,14 +47,14 @@ print(d)
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = [f'{human.name}-{human.age}' for human in humans]
+e = [f"{human.name}-{human.age}" for human in humans]
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [tuple([human.name, human.age]) for human in humans if human.age in range(27, 33)]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
